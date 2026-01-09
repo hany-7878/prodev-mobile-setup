95149d0427388cd059a07119b511959c323c9603
hany-7878 <htesfaye641@gmail.com>
2026-01-09 15:37:15 +0300

    last

--- prodev-mobile-app-0x03/app/index.tsx
+++ prodev-mobile-app-0x03/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
       <SafeAreaView style={styles.container}>
         <View style={styles.navGroup}>
           <Ionicons name="arrow-back" size={25} />
-          <Image source={require('../assets/images/logo.png')} />
+          <Image source={require('@/assets/images/logo.png')} />
         </View>
         <Text style={styles.largeText}>Sign in to your</Text>
         <Text style={styles.largeText}>Account</Text>
@@ -59,7 +59,7 @@ export default function Index() {
                 gap: 5,
               }}
             >
-              <Image source={require('../assets/images/google.png')} />
+              <Image source={require("@/assets/images/google.png")} />
               <Text style={styles.socialMediaButtonText}>
                 Continue with Google
               </Text>
@@ -74,7 +74,7 @@ export default function Index() {
                 gap: 5,
               }}
             >
-              <Image source={require('../assets/images/facebook.png')} />
+              <Image source={require("@/assets/images/facebook.png")} />
               <Text style={styles.socialMediaButtonText}>
                 Continue with Facebook
               </Text>
