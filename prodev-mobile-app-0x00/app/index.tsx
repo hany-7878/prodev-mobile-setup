e998324e67423b32cdd356ca2fde73de9d9b9fae
hany-7878 <htesfaye641@gmail.com>
2026-01-09 13:38:39 +0300

    updated

---
+++ prodev-mobile-app-0x00/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <text>Changes you make will automatically reload.</text>
+    </View>
+  );
+}
